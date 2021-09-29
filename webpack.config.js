ab30d8c0a1c266b9c390c52612b1a5d7154db798
dilog <dilog@rambler.ru>
2021-09-29 17:02:14 +0300

    Замена шрифтов

--- webpack.config.js
+++ webpack.config.js
@@ -129,7 +129,7 @@ module.exports = {
       },
 
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'file-loader',
         options: {
           name: 'fonts/[name].[ext]'
