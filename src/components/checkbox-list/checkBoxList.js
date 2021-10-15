b1436c4349e52216620574181839c26e380cdc92
dilog <dilog@rambler.ru>
2021-10-15 11:37:06 +0300

    Рефакторинг компонента checkbox-list

--- src/components/checkbox-list/checkBoxList.js
+++ src/components/checkbox-list/checkBoxList.js
@@ -1,50 +1,17 @@
-const checkBoxList = (args) => {
+import './checkbox-list.scss'
 
-  let {
-    id = null,
-    isExpand = false,
-  } = args;
+const checkBoxList = (node) => {
 
-  if (id === null) {
-    console.log("id не задан");
-    return -1;
+  const nodeClick = node.querySelector('[data-action=expand]');
+  const nodeList = node.querySelector('[data-action=expand-list]');
+
+  nodeClick.onclick=()=>{
+    nodeClick.classList.toggle('checkbox-list__ico_up');
+    nodeList.classList.toggle('checkbox-list__items_hide')
   }
-
-  const nodeCheckBoxList = document.getElementById(id);
-  const nodeOptions = nodeCheckBoxList.querySelector('.checkbox-list__items');
-
-  const nodeIco = nodeCheckBoxList.querySelector('[data-action=expand]').firstChild;
-
-  init();
-
-  function init() {
-    isExpand
-      ? nodeOptions.style.display = "block"
-      : nodeOptions.style.display = "none";
-    nodeCheckBoxList.addEventListener('click', headerClick);
-
-    isExpand
-      ? nodeIco.style.transform="rotate(-180deg)"
-      : nodeIco.style.transform="rotate(0deg)";
-  }
-
-  function headerClick(event){
-    event.target.dataset.action === 'expand'
-      ? toggleExpand()
-      : null;
-  }
-
-  function toggleExpand() {
-    isExpand = !isExpand;
-    isExpand
-      ? nodeOptions.style.display = "block"
-      : nodeOptions.style.display = "none";
-
-    isExpand
-      ? nodeIco.style.transform="rotate(-180deg)"
-      : nodeIco.style.transform="rotate(0deg)";
-  }
-
 };
 
-export default checkBoxList;
+(()=>{
+  const checkBoxListNodes=document.querySelectorAll('[data-type="checkbox-list"]');
+  checkBoxListNodes.forEach(((node)=>checkBoxList(node)));
+})();
