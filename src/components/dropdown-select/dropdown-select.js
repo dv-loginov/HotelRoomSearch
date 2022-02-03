549b6a23df57412fc983bdd8ebb3fafe3390f7e2
dilog <dilog@rambler.ru>
2022-02-03 18:07:15 +0300

    Рефакторинг компонентов dropdown, dropdown-select.

--- src/components/dropdown-select/dropdown-select.js
+++ src/components/dropdown-select/dropdown-select.js
@@ -1,25 +1,28 @@
 import './dropdown-select.scss'
 
-import {DropDownThings} from "../dropdown-select/classes/DropDownThigs";
-import {DropDownPeople} from "../dropdown-select/classes/DropDownPeople";
-
 const dropdownSelect = (node) => {
+
   const variant = node.dataset.variant;
   const buttonExpand = node.querySelector('[data-action=expand]');
-  const nodeOptionsWrap = node.querySelector('.dropdown__wrap-options');
+  // const nodeOptionsWrap = node.querySelector('.dropdown__wrap-options');
+
+  // console.log(node, variant, buttonExpand, nodeOptionsWrap);
+
   buttonExpand.onclick = () => {
-    nodeOptionsWrap.classList.toggle('dropdown__wrap-options-open');
+    // nodeOptionsWrap.classList.toggle('dropdown__wrap-options-open');
+    node.classList.toggle('dropdown_open');
   };
-  switch (variant) {
-    case 'things':
-      new DropDownThings(node);
-      break;
-    case 'people':
-      new DropDownPeople(node);
-      break;
-    default:
-      console.log('Ошибка: не могу найти подходяший елемент');
-  }
+
+  // switch (variant) {
+  //   case 'things':
+  //     new DropDownThings(node);
+  //     break;
+  //   case 'people':
+  //     new DropDownPeople(node);
+  //     break;
+  //   default:
+  //     console.log('Ошибка: не могу найти подходяший елемент');
+  // }
 };
 
 (() => {
